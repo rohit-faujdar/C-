007fa5e2d81d2256db3299fd9a261d8518bf5573
Rohit faujdar <sinsinwarrohitfaujdar@gmail.com>
2026-09-18 10:41:37 +0530

    add demo file

---
+++ Demo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i = 10;
+    long l = 7456;
+    float f = 12.5;
+    double d = 334.567;
+    char ch = 'a';
+    bool b = true;
+    cout << "integer = "<< i << "\n";
+    cout << "long = "<< l << "\n";
+    cout << "float = "<< f << "\n";
+    cout << "double = "<< d << "\n";
+    cout << "char = "<< ch << "\n";
+    cout << "boolean = "<< b << "\n";
+    return 0;
+}
